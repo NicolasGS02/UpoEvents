6ac97971af5aec8b5a45fdb2f5cb37fb867a06b9
Nicolás Gonzáles Sánchez <nicolasgonsan2004@gmail.com>
2025-06-05 01:16:08 +0200

    Arreglo Librerias

--- build/generated-sources/ap-source-output/Models/Usuarios_.java
+++ build/generated-sources/ap-source-output/Models/Usuarios_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2025-06-04T19:55:38")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2025-06-05T01:12:00")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
